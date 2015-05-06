4f989945eb007a6d681348680936576c3f91a962
khhle <khhle@ucsc.edu>
2015-05-06 00:07:48 -0700

    update working code

--- p5.py
+++ p5.py
@@ -1,93 +1,128 @@
+import sys
 import json
 from collections import namedtuple
 import heapq
 
 
-def start():
-    with open('Crafting.json') as f:
-        Crafting = json.load(f)
+def convert_to_tuple(inventory,items):
+    state = []
 
-    # List of items that can be in your inventory:
-    print Crafting['Items']
-    # example: ['bench', 'cart', ..., 'wood', 'wooden_axe', 'wooden_pickaxe']
-
-    # List of items in your initial inventory with amounts:
-    print Crafting['Initial']
-    # {'coal': 4, 'plank': 1}
-
-    # List of items needed to be in your inventory at the end of the plan:
-    # (okay to have more than this; some might be satisfied by initial inventory)
-    #print Crafting['Goal']
-    # {'stone_pickaxe': 2}
-
-    # Dictionary of crafting recipes:
-    print Crafting['Recipes']['craft stone_pickaxe at bench']
-    #print Crafting['Recipes'].items
-    # example:
-    # {	'Produces': {'stone_pickaxe': 1},
-    #	'Requires': {'bench': True},
-    #	'Consumes': {'cobble': 3, 'stick': 2},
-    #	'Time': 1
-    # }
-
-    Recipe = namedtuple('Recipe',['name','check','effect','cost'])
-    all_recipes = []
-    for name,rule in Crafting['Recipes'].items():
-        checker = make_checker(rule)
-        effector = make_effector(rule)
-        recipe = Recipe(name, checker, effector, rule['Time'])
-        all_recipes.append(recipe)
-
-    print all_recipes
+    for next in items:
+        if next in inventory:
+            state.append(inventory[next])
+        else:
+            state.append(0)
+    return tuple(state)
 
 
+def make_item_index_list(items):
+    index_list = {}
+    index = 0
+    for next in items:
+        index_list[next] = index
+        index += 1
+    return index_list
 
 
-
-
-
-    t_initial = 'a'
-    t_limit = 20
-    print "bcccccc"
-    edges = {'a': {'b':1,'c':10}, 'b':{'c':1}}
-    #print t_graph('a').next()
-    #print edges.items()
-    print search(t_graph, t_initial, t_is_goal, t_limit, t_heuristic)
+def graph(state):
+    for r in all_recipes:
+        if r.check(state):
+            print str(r.name) + ' ' + str(r.effect(state))
+            yield (r.name, r.effect(state), r.cost)
 
 
 def make_checker(rule):
      #this code runs once
 	 # do something with rule['Consumes'] and rule['Requires']
+    consumes = {}
+    requires = {}
+
+    if 'Consumes' in rule:
+        consumes = rule['Consumes']
+    if 'Requires' in rule:
+        requires = rule['Requires']
+
 
     def check(state):
         # this code runs millions of times
+        for next in consumes:
+            index = items_index_list[next]
+            if consumes[next] > state[index]:
+                return False
+
+        for next in requires:
+            index = items_index_list[next]
+            if requires[next] > state[index]:
+                return False
+
         return True # or False
 
     return check
 
 
 def make_effector(rule):
+
     # this code runs once
     # do something with rule['Produces'] and rule['Consumes']
-	def effect(state):
-		# this code runs millions of times
-		return state
 
-	return effect
+    consumes = {}
+    produces = {}
 
+    if 'Consumes' in rule:
+        consumes = rule['Consumes']
+    if 'Produces' in rule:
+        produces = rule['Produces']
+
+
+
+    def effect(state):
+        # this code runs millions of times
+        next_state = list(state)
+
+        for item in produces:
+            next_state[items_index_list[item]] += produces[item]
+
+        for item in consumes:
+            next_state[items_index_list[item]] -= consumes[item]
+
+        return tuple(next_state)
+
+    return effect
+
+
+def is_goal(current_state):
+    goal_state = convert_to_tuple(goal,items)
+    for i in range(0, len(goal_state)):
+        if current_state[i] < goal_state[i]:
+            return False
+    return True
+
+def heuristic(state):
+    #print "state " + str(state)
+
+
+    for item in non_consumables:
+        index = items_index_list[non_consumables[item]]
+        if state[index] > goal_state[index] and state[index] > 1:
+            return sys.maxint
+
+
+    return 0
 
 def search(graph, initial, is_goal, limit, heuristic):
     frontier = PriorityQueue()
     frontier.put(initial, 0)
     came_from = {}
     cost_so_far = {}
-    #verbose = {}
+    visited = {}
 
     came_from[initial] = None
     cost_so_far[initial] = 0
-    #verbose[initial] = ("no_action", initial, 0)
-
-    while not frontier.empty():
+    visited[initial] = ("Start", initial, 0)
+    i = 0
+    while not frontier.empty() and i < limit:
+        i += 1
+        #print i
         current = frontier.get()
 
         if is_goal(current):
@@ -95,9 +130,8 @@ def search(graph, initial, is_goal, limit, heuristic):
 
         neighborhood = graph(current)
 
-
         for next in neighborhood:
-
+            #print "next" + str(next)
             name, effect, neighborCost = next
             new_cost = cost_so_far[current] + neighborCost
 
@@ -106,48 +140,27 @@ def search(graph, initial, is_goal, limit, heuristic):
                 priority = new_cost + heuristic(effect)
                 frontier.put(effect, priority)
                 came_from[effect] = current
-                #verbose[effect] = next
+                visited[effect] = next
 
-    current = 'c'
-    path = [current]
+    if not is_goal(current):
+        return 0,[]
+
+
+    #if is_goal(current):
+    print i
+    plan = []
     total_cost = cost_so_far[current]
-    while current != 'a':
-
+    while current:
+        plan.append(visited[current])
         current = came_from[current]
-        path.append(current)
-
-    '''
-    if is_goal(current):
-        plan = []
-        #total_cost = cost[u]
-        while current:
-            plan.append(verbose[current])
-            current = came_from[current]
-        plan.reverse()
-        return 0, plan
-    else:
-        return 0, []
-    '''
-	#return cost_so_far, came_from, path
-    return total_cost, path
+    plan.reverse()
+    return total_cost, plan
 
 
 
 
-def t_graph(state):
-    edges = {'a': {'b':1,'c':10}, 'b':{'c':1}}
-    for next_state, cost in edges[state].items():
-        yield ((state,next_state), next_state, cost)
-
 
-def t_is_goal(state):
-	return state == 'c'
-
 
-def t_heuristic(state):
-	return 0
-
-#print search(t_graph, t_initial, t_is_goal, t_limit, t_heuristic)
 
 
 
@@ -167,6 +180,75 @@ class PriorityQueue:
         return heapq.heappop(self.elements)[1]
 
 
-if __name__ ==  '__main__':
 
-	start()
+all_recipes = []
+items_index_list = {}
+items = []
+consumables = []
+non_consumables = {}
+inventory = {}
+goal = {}
+initial_state = []
+goal_state = []
+
+#def start():
+with open('Crafting.json') as f:
+    Crafting = json.load(f)
+
+
+Recipe = namedtuple('Recipe',['name','check','effect','cost'])
+all_recipes = []
+for name,rule in Crafting['Recipes'].items():
+    print rule
+    checker = make_checker(rule)
+    effector = make_effector(rule)
+    recipe = Recipe(name, checker, effector, rule['Time'])
+    all_recipes.append(recipe)
+
+print "Recipes"  + str(all_recipes)
+
+
+
+# List of items that can be in your inventory:
+items = Crafting['Items']
+print "Items: " + str(items)
+
+#List of all non_consumable items
+temp = 0
+for i in [0, 1, 4, 6, 7, 12, 13, 15, 16]:
+    non_consumables[temp] = items[i]
+    temp += 1
+print 'Non Consumables: ' + str(non_consumables)
+
+# List of items in your initial inventory with amounts:
+inventory =  Crafting['Initial']
+print "Inventory: " + str(inventory)
+
+# List of items needed to be in your inventory at the end of the plan:
+goal = Crafting['Goal']
+print "Goal" + str(goal)
+
+goal_state = convert_to_tuple(goal,items)
+
+#keep track of items by index number
+items_index_list = make_item_index_list(items)
+#print items_index_list
+
+initial_state = convert_to_tuple(inventory,items)
+print "Initial state" + str(initial_state)
+
+
+
+total_cost = 0
+path = []
+total_cost, path = search(graph, initial_state, is_goal, 1000000, heuristic)
+print "Total cost: " + str(total_cost)
+print "Path: " + str(path)
+
+
+
+
+
+
+
+
